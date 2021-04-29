148164fc548ebf1cf218abe93609a4d8a9629b35
Matthias <mattwi@fysik.dtu.dk>
2021-04-29 17:27:40 +0200

    Fix feltordiag.sh

--- feltordiag.sh
+++ feltordiag.sh
@@ -4,6 +4,6 @@
 # If feltor is not here then change the FELTOR_PATH enviromnent variable
 # export FELTOR_PATH="path/to/feltor"
 
-make -C $FELTOR_PATH/src/feltor/feltordiag
+make -C $FELTOR_PATH/src/feltor feltordiag
 
-$FELTOR_PATH/src/feltor/feltordiag $1 > "$2"
+$FELTOR_PATH/src/feltor/feltordiag $1 $2
