e485a3f3385e9956506ba532578ee078f9c071aa
Matthias <mattwi@fysik.dtu.dk>
2021-08-12 14:19:45 +0200

    Change default job accout to tsvv3

--- job.sh
+++ job.sh
@@ -3,7 +3,7 @@
 #SBATCH -N 4 -n 16 --ntasks-per-node=4 --exclusive
 #SBATCH --gres=gpu:4
 #SBATCH --partition=m100_fua_prod
-#SBATCH --account=fuac5_feltor
+#SBATCH --account=fuac5_tsvv3
 #SBATCH --time=24:00:00 # 24 hours is maximum
 
 echo "Marconi 100 cluster with Tesla V100 GPUs"
