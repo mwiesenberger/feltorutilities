7427d8557637d34b9e0ee53f3388ca39ab59a145
Matthias Wiesenberger <mwiesenb@login02.leonardo.local>
2023-11-10 15:00:05 +0100

    Update job.sh to leonardo

--- job.sh
+++ job.sh
@@ -3,20 +3,21 @@
 #SBATCH -N 4 -n 16 --ntasks-per-node=4 --exclusive
 #####SBATCH --gres=gpu:4
 #SBATCH --gpus-per-node=4
-#SBATCH --partition=m100_fua_prod
-#SBATCH --account=fuac7_tsvv3
+#SBATCH --partition=boost_fua_prod
+#SBATCH --account=fual7_feltor
 #SBATCH --time=24:00:00 # 24 hours is maximum
 
-echo "Marconi 100 cluster with Tesla V100 GPUs"
+echo "Leonardo cluster with Tesla A100 GPUs"
 
 hostname
 date
 module list
+spack find jsoncpp
 echo "$@"
 
 : ${FELTOR_PATH:="../feltor"}
 
 # $@ forwards all arguments
-echo "1 1 16" | mpirun -gpu -n 16 $FELTOR_PATH/src/feltor/feltor_mpi "$@"
+echo "1 1 16" | mpirun -n 16 $FELTOR_PATH/src/feltor/feltor_mpi "$@"
 
 date
